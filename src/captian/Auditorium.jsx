27976e81fc2e06489fff982af9ab4b04b7aeb2b9
shaanis <mhdshanis3@gmail.com>
2025-03-27 11:04:36 +0530

    float btn

--- src/captian/Auditorium.jsx
+++ src/captian/Auditorium.jsx
@@ -121,8 +121,8 @@ const Auditorium = ({ height, width }) => {
         </div>
 
         <button
-          className="btn btn-success d-flex align-items-center justify-content-center rounded-circle"
-          style={{ position: "fixed", bottom: "120px", right: "15px",width:'40px',height:"40px" }}
+          className="btn btn-success fs-4 d-flex align-items-center justify-content-center rounded-circle"
+          style={{ position: "fixed", bottom: "120px", right: "15px",width:'45px',height:"45px" }}
         >
           +
         </button>
